a26117548d7cc77783531d932ef49a8a76709687
Jongseok Kwon <jjijji5728@tukorea.ac.kr>
2023-11-10 23:02:58 +0900

    test: 입력받은 날짜가 1~31 범위를 벗어나면 예외처리 테스트코드 작성

---
+++ src/test/java/christmas/domain/DayTest.java
@@ -0,0 +1,18 @@
+package christmas.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class DayTest {
+    @ParameterizedTest
+    @ValueSource(ints = {-1,0,32})
+    @DisplayName("12월일 때 1~31의 숫자가 아닌 경우 예외를 처리한다.")
+    void createDayByWrongNumber(int number){
+        assertThatThrownBy(() -> new Day(number))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
